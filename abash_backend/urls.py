d88ab4535ef702659988a0ad202d7b6f75db0290
Mishal Al Rahman <mishal@iut-dhaka.edu>
2021-10-04 01:38:15 +0600

    messed

--- abash_backend/urls.py
+++ abash_backend/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 admin.site.site_header = 'Abash Admin'
 
@@ -26,4 +28,6 @@ urlpatterns = [
     path('playground/', include('playground.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
     path('abash/', include('abash.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
